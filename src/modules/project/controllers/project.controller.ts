9a7037f3818f6c921111382f261b47234e276989
eeuhhnil <144205221+eeuhhnil@users.noreply.github.com>
2025-02-14 15:49:51 +0700

    Initial commit

--- src/modules/project/controllers/project.controller.ts
+++ src/modules/project/controllers/project.controller.ts
@@ -1,9 +1,6 @@
-import { Body, Controller, Delete, Param, Post } from "@nestjs/common";
-import { ApiBearerAuth, ApiOperation, ApiTags } from "@nestjs/swagger";
+import { ApiBearerAuth, ApiTags } from "@nestjs/swagger";
 import { ProjectMemberService, ProjectService } from "../services";
-import { AuthUser } from "../../../common/auth/decorators";
-import { AuthPayload } from "../../../common/auth/types";
-import { CreateProjectDto } from "../dtos/create-project.dto";
+import { Controller } from "@nestjs/common";
 
 @Controller("project")
 @ApiTags("project")
@@ -13,13 +10,4 @@ export class ProjectController {
     private readonly projectService: ProjectService,
     private readonly projectMemberService: ProjectMemberService
   ) {}
-
-  @Post()
-  @ApiOperation({ summary: "Create project" })
-  async createProject(@Param("spaceId") spaceId: string, @AuthUser() authUser: AuthPayload, @Body() body: CreateProjectDto) {
-    return this.projectService.createProject(spaceId, authUser.sub, body);
-  }
-
-  // @Delete(':id')
-  // async deleteProject(){}
 }
--- src/modules/project/dtos/create-project.dto.ts
+++ src/modules/project/dtos/create-project.dto.ts
@@ -1,18 +1,1 @@
-import { ApiProperty } from "@nestjs/swagger";
-import { IsNotEmpty, IsString } from "class-validator";
-
-export class CreateProjectDto {
-  @ApiProperty({
-    example: "project",
-  })
-  @IsString()
-  @IsNotEmpty()
-  name: string;
-
-  @ApiProperty({
-    example: "project description",
-  })
-  @IsString()
-  @IsNotEmpty()
-  desc: string;
-}
+export class CreateProjectDto {}
